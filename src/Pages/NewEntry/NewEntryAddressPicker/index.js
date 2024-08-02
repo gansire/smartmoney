b991ed78a13bfb8358c86b9e07598a5192f3c177
gansire <jean16203@gmail.com>
2024-08-01 21:21:23 -0300

    Adicionando Geolocalização no Aplicativo - Parte 2

--- src/Pages/NewEntry/NewEntryAddressPicker/index.js
+++ src/Pages/NewEntry/NewEntryAddressPicker/index.js
@@ -3,17 +3,34 @@ import { StyleSheet, TouchableOpacity, View, Alert } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Colors from '../../../styles/Colors';
+import Geocoder from 'react-native-geocoding';
+
+const getLocation = (latitude, longitude) => {
+	Geocoder.init('AIzaSyB5MVJft0UNZs282kntXeBgjuzfvEhjlK4');
+
+	Geocoder.from({ latitude, longitude }).then(json => {
+		const formattedAddress = json.results[0].formatted_address;
+		Alert.alert('Endereço formatado', formattedAddress);
+	}).catch(error => {
+		console.error("NewEntryAddressPicker:: getLocation :: erro ao recuperar a localização", error);
+		Alert.alert('Houve um erro ao recuperar a posição, por favor, tenha certeza que autorizou este aplicativo');
+	});
+}
+
+const getPosition = () => {
+	Geolocation.getCurrentPosition(pos => {
+		const latitude = pos.coords.latitude;
+		const longitude = pos.coords.longitude;
+		getLocation(latitude, longitude);
+	}, error => {
+		console.error("NewEntryAddressPicker:: getPosition :: erro ao recuperar a posição", error);
+	});
+	Alert.alert('Houve um erro ao recuperar a posição, por favor, tenha certeza que autorizou este aplicativo');
+}
 
 const NewEntryAddressPicker = () => {
 	const onButtonPress = () => {
-		Geolocation.getCurrentPosition(pos => {
-			const latitude = pos.coords.latitude;
-			const longitude = pos.coords.longitude;
-			const position = `Lat. ${latitude} - Lon. ${longitude}`;
-			Alert.alert("Posição", position)
-		}, error =>{
-			console.error("NewEntryAddressPicker :: erro ao recuperar a posição", error);
-		});
+		getPosition()
 	}
 
 	return (
